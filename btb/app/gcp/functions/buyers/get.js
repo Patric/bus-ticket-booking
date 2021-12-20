a7954fa71d5718ae98b80a73b9e3f871000efac9
Patryk Sulej <p.sulej@prometheus-sa.com>
2021-12-20 01:40:14 +0100

    get

--- btb/app/gcp/functions/buyers/get.js
+++ btb/app/gcp/functions/buyers/get.js
@@ -1,32 +1,34 @@
 const { getFirestore } = require('firebase-admin/firestore');
 
-export const trigger = (req, res) => {
-    const id = req.params.id;
-  
-    const COLLECTION_NAME = 'Buyers';
-    const firestore = getFirestore();
-    
-    firestore.collection(COLLECTION_NAME)
-      .doc(id)
-      .get()
-      .then(doc => {
-        if (!(doc && doc.exists)) {
-          res.status(404).send({
-            error: 'Unable to find the document'
+module.exports = {
+    trigger: (req, res) => {
+        const id = req.params.id;
+      
+        const COLLECTION_NAME = 'Buyers';
+        const firestore = getFirestore();
+        
+        firestore.collection(COLLECTION_NAME)
+          .doc(id)
+          .get()
+          .then(doc => {
+            if (!(doc && doc.exists)) {
+              res.status(404).send({
+                error: 'Unable to find the document'
+              });
+            }
+            const data = doc.data();
+            if (!data) {
+              res.status(404).send({
+                error: 'Found document is empty'
+              });
+            }
+            res.status(200).send(data);
+          }).catch(err => {
+            console.error(err);
+            res.status(404).send({
+              error: 'Unable to retrieve the document',
+              err
+            });
           });
-        }
-        const data = doc.data();
-        if (!data) {
-          res.status(404).send({
-            error: 'Found document is empty'
-          });
-        }
-        res.status(200).send(data);
-      }).catch(err => {
-        console.error(err);
-        res.status(404).send({
-          error: 'Unable to retrieve the document',
-          err
-        });
-      });
-  }
+      }
+  };
