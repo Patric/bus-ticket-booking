00fa9068e3cd55a24988ecca94ddcd60f68a6027
Patryk Sulej <p.sulej@prometheus-sa.com>
2022-01-03 20:25:38 +0100

    test---

--- btb/app/gcp/functions/buyers/post.js
+++ btb/app/gcp/functions/buyers/post.js
@@ -2,12 +2,8 @@ const { getFirestore } = require('firebase-admin/firestore');
 
 module.exports = {
     trigger: (req, res) => {
-      console.log(req.body);
-
         const COLLECTION_NAME = 'Buyers';
         const firestore = getFirestore();
-        console.log(req.body);
-
         firestore.collection(COLLECTION_NAME)
           .doc('fdas').get().then( () => res.send("GOT"))
       }
