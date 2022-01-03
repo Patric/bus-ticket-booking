ca28b23c526bf48da95112814ec21e9f4b59002b
Patryk Sulej <p.sulej@prometheus-sa.com>
2022-01-03 19:35:53 +0100

    psottry2

--- btb/app/gcp/functions/buyers/post.js
+++ btb/app/gcp/functions/buyers/post.js
@@ -9,6 +9,25 @@ module.exports = {
           .add({
             email: req.body.email,
             person_id: req.body.person_id})
-          .then(res => res.send("DODANO"))
+          .then(doc => {
+            if (!(doc && doc.exists)) {
+              res.status(404).send({
+                error: 'Unable to find the document'
+              });
+            }
+            const data = doc.data();
+            if (!data) {
+              res.status(404).send({
+                error: 'Found document is empty'
+              });
+            }
+            res.status(200).send(data);
+          }).catch(err => {
+            console.error(err);
+            res.status(404).send({
+              error: 'Unable to retrieve the document',
+              err
+            });
+          });
       }
   };
