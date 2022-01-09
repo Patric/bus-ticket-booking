8f99741baf580d3656fdf78608c22ebd3dc9e746
Patryk Sulej <p.sulej@prometheus-sa.com>
2022-01-09 14:26:39 +0100

    test-auth

--- btb/app/gcp/functions/auth/post-auth.js
+++ btb/app/gcp/functions/auth/post-auth.js
@@ -13,21 +13,21 @@ function handleImplicitAuthRequest(req, res) {
     }));
   }
 
-  const firestore = getFirestore();
-  firestore.collection('Clients')
-    .where('client_id', '==', req.body.client_id)
-    .where('redirect_url', '==', req.body.redirect_url)
-    .get()
-    .then(querySnapshot => {
-      if (querySnapshot.empty) {
-        console.error(err);
-        res.status(400).send({
-          error: 'Invalid client/redirect URL.',
-          err
-        });
-      }
-    })
-    .then(() => {
+  // const firestore = getFirestore();
+  // firestore.collection('Clients')
+  //   .where('client_id', '==', req.body.client_id)
+  //   .where('redirect_url', '==', req.body.redirect_url)
+  //   .get()
+  //   .then(querySnapshot => {
+  //     if (querySnapshot.empty) {
+  //       console.error(err);
+  //       res.status(400).send({
+  //         error: 'Invalid client/redirect URL.',
+  //         err
+  //       });
+  //     }
+  //   })
+    // .then(() => {
       const html = pug.renderFile(path.join(__dirname, 'auth.pug'), {
         response_type: 'code',
         client_id: req.body.client_id,
@@ -35,17 +35,17 @@ function handleImplicitAuthRequest(req, res) {
         code_challenge: req.body.code_challenge
       });
       res.status(200).send(html);
-    })
-    .catch(error => {
-      if (error.message === 'Invalid client/redirect URL.') {
-        res.status(400).send(JSON.stringify({
-          'error': 'access_denied',
-          'error_description': error.message
-        }));
-      } else {
-        throw error;
-      }
-    });
+    // })
+    // .catch(error => {
+    //   if (error.message === 'Invalid client/redirect URL.') {
+    //     res.status(400).send(JSON.stringify({
+    //       'error': 'access_denied',
+    //       'error_description': error.message
+    //     }));
+    //   } else {
+    //     throw error;
+    //   }
+    // });
 }
 
 module.exports = {
