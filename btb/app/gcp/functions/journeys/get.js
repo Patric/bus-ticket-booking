0fc4581d82428b562824144fb31e64901bcb0451
Patryk Sulej <p.sulej@prometheus-sa.com>
2022-01-10 23:29:31 +0100

    journeys fix

--- btb/app/gcp/functions/journeys/get.js
+++ btb/app/gcp/functions/journeys/get.js
@@ -1,13 +1,17 @@
 const e = require('express');
 const { getFirestore } = require('firebase-admin/firestore');
 
+class JourneyDTO {
+  constructor(date, line)
+}
+
 module.exports = {
     trigger: (req, res) => {
         const firestore = getFirestore();
         const from = req.query.from;
         const to = req.query.to;
         const date = req.query.date;
-        
+
         firestore.collection('Journeys').where('date', '==', new Date(date).toDateString())
           .get()
           .then(querySnapshot => {
@@ -16,8 +20,12 @@ module.exports = {
                 error: 'Unable to find the document'
               });
             }
-            
-            const data = querySnapshot.docs.map(doc => doc.data());
+            const data = querySnapshot.docs.map(doc => {
+            const line_qs_data = await firestore.collection('Lines').doc(doc.data().line_id).get().docs[0].data();
+            if (line_qs_data.stationFrom === from && line_qs_data.stationTo === to) {
+               return new JourneyDTO(line_qs.docs[0].data(), date);
+            }
+            });
             res.status(200).send(data);
           }).catch(err => {
             console.error(err);
