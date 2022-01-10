7c527fdcdd21fa3e11b0c8f0fcc80e8c011b316e
Patryk Sulej <p.sulej@prometheus-sa.com>
2022-01-10 23:41:21 +0100

    datafix

--- btb/app/gcp/functions/journeys/get.js
+++ btb/app/gcp/functions/journeys/get.js
@@ -1,8 +1,10 @@
 const e = require('express');
-const { getFirestore } = require('firebase-admin/firestore');
+const {
+  getFirestore
+} = require('firebase-admin/firestore');
 
 class JourneyDTO {
-  constructor(date, line){
+  constructor(date, line) {
     this.date = date;
     this.line = line;
   }
@@ -18,24 +20,27 @@ module.exports = {
         firestore.collection('Journeys').where('date', '==', new Date(date).toDateString())
           .get()
           .then(querySnapshot => {
-            if (querySnapshot.empty) {
-              res.status(404).send({
-                error: 'Unable to find the document'
+              if (querySnapshot.empty) {
+                res.status(404).send({
+                  error: 'Unable to find the document'
+                });
+              }
+              const data = querySnapshot.docs.map(doc => {
+                firestore.collection('Lines').doc(doc.data().line_id).get()
+                  .then(line_qs => {
+                    const line_qs_data = line_qs.docs[0].data();
+                    if (line_qs_data.stationFrom === from && line_qs_data.stationTo === to) {
+                      return new JourneyDTO(line_qs.docs[0].data(), date);
+                    }
+                  });
+              });
+                res.status(200).send(data);
+              }).catch(err => {
+                console.error(err);
+                res.status(404).send({
+                  error: 'Unable to retrieve the document',
+                  err
+                });
               });
             }
-            const data = querySnapshot.docs.map(doc => {
-            const line_qs_data = await firestore.collection('Lines').doc(doc.data().line_id).get().docs[0].data();
-            if (line_qs_data.stationFrom === from && line_qs_data.stationTo === to) {
-               return new JourneyDTO(line_qs.docs[0].data(), date);
-            }
-            });
-            res.status(200).send(data);
-          }).catch(err => {
-            console.error(err);
-            res.status(404).send({
-              error: 'Unable to retrieve the document',
-              err
-            });
-          });
-      }
-  };
+          };
