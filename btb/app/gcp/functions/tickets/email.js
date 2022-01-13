c30a9302c0c0f8a4d31926b71af58f16c8667661
Patryk Sulej <p.sulej@prometheus-sa.com>
2022-01-13 23:04:04 +0100

    attachfile

--- btb/app/gcp/functions/tickets/email.js
+++ btb/app/gcp/functions/tickets/email.js
@@ -2,13 +2,18 @@
  const smtpTransport = nodemailer.createTransport("smtps://btbsystemproject%40gmail.com:"+encodeURIComponent(atob('V2lla3N6eVNhbW9jaG9kQmlsZXRaYW1hd2lhbmllUHJvamVrdC4xMjM=')) + "@smtp.gmail.com:465"); 
 
  module.exports = {
-     send: (recipient, subject, htmlMessage) => {
+     send: (recipient, subject, htmlMessage, attachmentPath, attachmentFilename,  attachmentId) => {
 
          const mailOptions = {
              from: "BusTicketBooking System ✔ <btbsystemproject@gmail.com>", // sender address
              to: recipient, // list of receivers
              subject: subject, // Subject line
-             html: htmlMessage // html body
+             html: htmlMessage,
+             attachments: [{
+                filename: attachmentFilename,
+                path:  attachmentPath,
+                cid: attachmentId //same cid value as in the html img src
+            }] // html body
          }
 
          smtpTransport.sendMail(mailOptions, function (error, response) {
